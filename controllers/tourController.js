6fdb55d9d79ae1f11ef5cd2de6a3b3591ed65f47
Shamil <shamil.isayev95@gmail.com>
2022-01-23 21:49:53 +0400

    added tour statistics and tour busiestMonth, aggregation pipeline and etc..

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,6 +9,66 @@ exports.getFiveCheapTours = (req, res, next) => {
   next();
 }
 
+exports.getToursStatistics = async (req, res) => {
+  try {
+    const tourStatistics = await Tours.aggregate([
+      {$match: {ratingsAverage: {$gte: 4.5}}},
+      {
+        $group: {
+          _id: '$difficulty',
+          avgRating: {$avg: "$ratingsAverage"},
+          avgPrice: {$avg: "$price"},
+          minPrice: {$min: "$price"},
+          maxPrice: {$max: "$price"},
+        }
+      },
+      {$sort: {avgPrice: 1}}
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {tourStatistics}
+    })
+  } catch (err) {
+    console.log(err, 'error')
+  }
+}
+
+exports.getBusyestMonth = async (req, res) => {
+  try {
+    const year = req.params.year * 1
+
+    const busyestMonth = await Tours.aggregate([
+      {$unwind: '$startDates'},
+      {$match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          }
+        }
+        },
+      {$group: {
+        _id: {$month: '$startDates'},
+          sumOfToursForMonth: {$sum: 1},
+          tour: {$push: '$name'}
+        }},
+      {$addFields: {month: `$_id`}},
+      {$project: {_id: 0}},
+      {$sort: {sumOfToursForMonth: -1}},
+      {$limit: 12}
+    ])
+
+    res.status(200).json({
+      status: 'success',
+      data: {busyestMonth}
+    })
+  } catch (err) {
+    res.status(401).json({
+      status: 'fail',
+    })
+  }
+}
+
 exports.getAllTours = async (req, res) => {
   try {
     const features = new ApiFeatures(Tours.find(), req.query).filter().sort().limitFields().paginate()
--- router/tourRoute.js
+++ router/tourRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
-const {getAllTours, getTour, createTour, deleteTour, updateTour, getFiveCheapTours} = require('../controllers/tourController');
+const {getAllTours, getTour, createTour, deleteTour, updateTour, getFiveCheapTours, getToursStatistics, getBusyestMonth} = require('../controllers/tourController');
 
 const router = express.Router();
 
 router.route('/top-5-cheap').get(getFiveCheapTours, getAllTours)
+router.route('/statistics').get(getToursStatistics)
+router.route('/busyestMonth/:year').get(getBusyestMonth)
 router.route('/').get(getAllTours).post(createTour);
 
 router.route('/:id').get(getTour).delete(deleteTour).patch(updateTour);
